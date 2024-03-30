227ab2e867702af1c493547bbd3388e6f0aada49
ActiveXHealth <165322665+ActiveXHealth@users.noreply.github.com>
2024-03-30 01:12:39 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
-var modoOscuroo = false;
+var modoOscuroo = true;
+window.addEventListener('load', function() {
+    // Llamar a la función cuando todos los recursos (incluyendo imágenes y scripts) estén completamente cargados
+    modoOscuro();
+  });
+var toggle = true;
 const navItem = document.querySelectorAll(".nav-item");
     navItem.forEach(item => {
         item.classList.add("light-mode");
     });
+
 
 function modo() {
     modoOscuroo = !modoOscuroo;
@@ -11,7 +17,7 @@ function modo() {
         const imgLuna = document.getElementById("imgLuna");
         imgLuna.classList.add("dark-mode");
         imgLuna.classList.remove("light-mode");
-        imgLuna.src="/assets/img/sun.svg";
+        imgLuna.src="assets/img/sun.svg";
         btnLuna.textContent = "Modo Claro";
         modoOscuro();
     } else {
@@ -20,7 +26,7 @@ function modo() {
         const imgLuna = document.getElementById("imgLuna");
         imgLuna.classList.add("light-mode");
         imgLuna.classList.remove("dark-mode");
-        imgLuna.src="/assets/img/moon-fill.svg";
+        imgLuna.src="assets/img/moon.svg";
         btnLuna.textContent = "Modo Oscuro";
     }
 }
@@ -31,13 +37,28 @@ const modoOscuro = () => {
     const btnRegistrarse = document.querySelector(".btnRegistrarse");
     const navItem = document.querySelectorAll(".nav-link");
     const configIcon = document.getElementById("configIcon");
-    configIcon.src = "/assets/img/gear_white.svg";
+    const cards = document.querySelectorAll(".card");
+    const cardTitles = document.querySelectorAll(".card-title");
+    const cardsTexts = document.querySelectorAll(".card-text");
+    const saludo = document.querySelector(".saludo");
+    configIcon.src = "assets/img/gear_white.svg";
     document.body.classList.add("dark-mode");
     document.body.classList.remove("light-mode");
     navItem.forEach(item => {
         item.classList.add("dark-mode");
         item.classList.remove("light-mode");
     });
+    cards.forEach(item => {
+        item.classList.remove("bg-light");
+        item.classList.add("bg-dark");
+    });
+    cardTitles.forEach(item => {
+        item.setAttribute("style","color:white;");
+    });
+    cardsTexts.forEach(item => {
+        item.setAttribute("style","color:white;");
+    });
+    saludo.setAttribute("style","color:white;")
     btnIngresar.classList.replace("btn-outline-dark", "btn-outline-light");
     btnRegistrarse.classList.replace("btn-outline-dark", "btn-outline-light");
     navbar.setAttribute("data-bs-theme", "dark");
@@ -50,18 +71,34 @@ const modoClaro = () => {
     const btnIngresar = document.querySelector(".btnIngresar");
     const btnRegistrarse = document.querySelector(".btnRegistrarse");
     const navItem = document.querySelectorAll(".nav-link");
-    configIcon.src = "/assets/img/gear.svg";
+    const cards = document.querySelectorAll(".card");
+    const cardTitles = document.querySelectorAll(".card-title");
+    const cardsTexts = document.querySelectorAll(".card-text");
+    const saludo = document.querySelector(".saludo");
+    configIcon.src = "assets/img/gear.svg";
     document.body.classList.remove("dark-mode");
     document.body.classList.add("light-mode");
     navItem.forEach(item => {
         item.classList.remove("dark-mode");
         item.classList.add("light-mode");
     });
+    cards.forEach(item => {
+        item.classList.remove("bg-dark");
+        item.classList.add("bg-light");
+    });
+    cardTitles.forEach(item => {
+        item.setAttribute("style","'color:black;'");
+    });
+    cardsTexts.forEach(item => {
+        item.setAttribute("style","'color:black;'");
+    });
+    saludo.setAttribute("style","'color:black;'")
     btnIngresar.classList.replace("btn-outline-light","btn-outline-dark");
     btnRegistrarse.classList.replace("btn-outline-light","btn-outline-dark");
     navbar.setAttribute("data-bs-theme", "light");
+    navbar.removeAttribute("data-bs-theme");
     navbar.classList.remove("bg-dark");
-    navbar.classList.add("bg-light");
+    navbar.classList.add("light-mode");
 }
 
 const registro = () => {
@@ -70,4 +107,20 @@ const registro = () => {
 
 const ingreso = () => {
     alert("*ingresa*");
+}
+
+const estructurarConfigIcon = () => {
+    const canvas = document.getElementById("offcanvasNavbar");
+    const configIcon = document.querySelector(".dropdownsito");
+
+    // Agregar un oyente de eventos para detectar cuando el offcanvas se cierra completamente
+    canvas.addEventListener('hidden.bs.offcanvas', function () {
+        configIcon.classList.add("dropdown-menu-end");
+        canvas.style.maxWidth = "100%"; // Establecer maxWidth en 100% cuando se cierra el offcanvas
+    });
+
+    if (canvas.classList.contains("show") || canvas.classList.contains("showing")) {
+        configIcon.classList.remove("dropdown-menu-end");
+        canvas.style.maxWidth = "75%";
+    }
 }
