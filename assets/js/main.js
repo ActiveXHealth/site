32057eea3fc46566b7fcac343aae7c5f006ead28
ActiveXHealth <165322665+ActiveXHealth@users.noreply.github.com>
2024-04-17 13:32:49 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,13 +14,16 @@ function mostrar (id) {
 //Cambiar de tema
 var toggleModo = false;
 
-const btnOscuro = document.querySelector(".btnTemaOscuro");
-const parrafo = document.getElementById("hola");
+document.getElementById("containerPresentacion");
 const modoOscuro = () => {
     toggleModo = !toggleModo;
     if (toggleModo){
         //Modo oscuro
+        alert("modo oscuro aktibado");
+        containerPresentacion.style.backgroundImage ="linear-gradient(to bottom, rgb(121, 89, 212), rgb(76, 55, 135))";
     } else {
         //Modo claro
+        alert("modo claro aktibado");
+        btnUno.classList.remove("bg-dark");
     }
 }
