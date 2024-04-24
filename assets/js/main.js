0b620afcaec90e6cc1e69ec1ba82e7c34bc2c390
ActiveXHealth <165322665+ActiveXHealth@users.noreply.github.com>
2024-04-24 13:32:56 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,11 +19,9 @@ const modoOscuro = () => {
     toggleModo = !toggleModo;
     if (toggleModo){
         //Modo oscuro
-        alert("modo oscuro aktibado");
         containerPresentacion.style.backgroundImage ="linear-gradient(to bottom, rgb(121, 89, 212), rgb(76, 55, 135))";
     } else {
         //Modo claro
-        alert("modo claro aktibado");
-        btnUno.classList.remove("bg-dark");
+        containerPresentacion.style.backgroundImage ="linear-gradient(to bottom, var(--azul), rgb(62, 132, 136))";
     }
 }
